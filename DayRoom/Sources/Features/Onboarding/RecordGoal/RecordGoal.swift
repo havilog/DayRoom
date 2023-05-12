e94d837cd209e52792a88c5c780dd25efcd1e2ac
SangJin Han <s202188@naver.com>
2023-05-12 14:03:37 +0900

    RecordGaol 구현 중

--- DayRoom/Sources/Features/Onboarding/RecordGoal/RecordGoal.swift
+++ DayRoom/Sources/Features/Onboarding/RecordGoal/RecordGoal.swift
@@ -5,6 +5,7 @@
 //  Created by 한상진 on 2023/05/12.
 //
 
+import Foundation
 import ComposableArchitecture
 
 struct RecordGoal: Reducer {
@@ -12,13 +13,40 @@ struct RecordGoal: Reducer {
     // MARK: State
     
     struct State: Hashable {
+        var isCustomValid: Bool = false
+        var isCompleteButtonDisabled: Bool = false //  { !isCustomValid || custom.isEmpty }
         
+        @BindingState var custom: String = ""
+        @BindingState var goal: GoalOfRecord?
+    }
+    
+    enum GoalOfRecord: Hashable {
+        case everytime
+        case memory
+        case daily
+        case emotion
+        case longliving
+        case custom
+        
+        var description: String {
+            switch self {
+            case .everytime: return "모든 순간을 기록하기"
+            case .memory: return "추억을 간직하기"
+            case .daily: return "일상을 기록하기"
+            case .emotion: return "감정을 기록하기"
+            case .longliving: return "오랫동안 기억하기"
+            case .custom: return "직접 입력하기"
+            }
+        }
     }
     
     // MARK: Action
     
-    enum Action: Equatable {
+    enum Action: Equatable, BindableAction {
+        case backButtonTapped
+        case completeButtonTapped
         
+        case binding(BindingAction<State>)
     }
     
     // MARK: Dependency
@@ -26,13 +54,31 @@ struct RecordGoal: Reducer {
     // MARK: Body
     
     var body: some ReducerOf<Self> {
+        BindingReducer()
         Reduce(core)
     }
     
     func core(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
+        case .binding(\.$custom):
+            state.isCustomValid = validate(custom: state.custom)
+            return .none
             
+        case .binding:
+            return .none
+            
+        case .backButtonTapped:
+            return .none
+            
+        case .completeButtonTapped:
+            return .none
         }
+    }
+    
+    private func validate(custom: String) -> Bool {
+        let regex = "[A-Za-z0-9]{0,12}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: custom)
     }
 }
 
