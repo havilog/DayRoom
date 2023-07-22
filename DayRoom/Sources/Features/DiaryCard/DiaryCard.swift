1b307ed5bf62e0fedc7542e0c170ff7a95529f8e
SangJin Han <s202188@naver.com>
2023-07-22 17:28:13 +0900

    가로이미지 세로로 들어가는 이슈 수정

--- DayRoom/Sources/Features/DiaryCard/DiaryCard.swift
+++ DayRoom/Sources/Features/DiaryCard/DiaryCard.swift
@@ -95,7 +95,8 @@ struct DiaryCard: Reducer {
             
         case .binding(\.$selectedImageItem):
             return .run { [imageItem = state.selectedImageItem] send in
-                let image = try await imageItem?.loadTransferable(type: Image.self) 
+                let uiImage = try await imageItem?.loadTransferable(type: UIImage.self) ?? UIImage()
+                let image = Image(uiImage: uiImage)
                 await send(.imageItemSelected(image: image))
                 await send(.delegate(.imageSelected))
             }
@@ -308,5 +309,60 @@ struct DiaryCardView_Previews: PreviewProvider {
             )
         )
         .previewDisplayName("일기")
+    }
+}
+
+extension UIImage: Transferable {
+    public static var transferRepresentation: some TransferRepresentation {
+        DataRepresentation(importedContentType: .image) { data in
+            guard let uiImage = UIImage(data: data) else { return UIImage() }
+            return uiImage.fixedOrientation
+        }
+    }
+}
+
+extension UIImage {
+    var fixedOrientation: UIImage {
+        guard imageOrientation != .up else { return self }
+        
+        var transform: CGAffineTransform = .identity
+        switch imageOrientation {
+        case .down, .downMirrored:
+            transform = transform
+                .translatedBy(x: size.width, y: size.height).rotated(by: .pi)
+        case .left, .leftMirrored:
+            transform = transform
+                .translatedBy(x: size.width, y: 0).rotated(by: .pi)
+        case .right, .rightMirrored:
+            transform = transform
+                .translatedBy(x: 0, y: size.height).rotated(by: -.pi/2)
+        case .upMirrored:
+            transform = transform
+                .translatedBy(x: size.width, y: 0).scaledBy(x: -1, y: 1)
+        default:
+            break
+        }
+        
+        guard
+            let cgImage = cgImage,
+            let colorSpace = cgImage.colorSpace,
+            let context = CGContext(
+                data: nil, width: Int(size.width), height: Int(size.height),
+                bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0,
+                space: colorSpace, bitmapInfo: cgImage.bitmapInfo.rawValue
+            )
+        else { return self }
+        context.concatenate(transform)
+        
+        var rect: CGRect
+        switch imageOrientation {
+        case .left, .leftMirrored, .right, .rightMirrored:
+            rect = CGRect(x: 0, y: 0, width: size.height, height: size.width)
+        default:
+            rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        }
+        
+        context.draw(cgImage, in: rect)
+        return context.makeImage().map { UIImage(cgImage: $0) } ?? self
     }
 }
